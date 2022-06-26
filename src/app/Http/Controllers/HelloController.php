12ffa3362a71fa280c10222f9876b1763d0a0d58
{ayaka-fujiyoshi} <ayaka1712pome@gmail.com>
2022-06-26 21:21:43 +0900

    ビューとテンプレートまで

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -4,146 +4,46 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// 第三章
+// //コントローラーでテンプレートを使う
 // class HelloController extends Controller
 // {
 //     public function index() {
-//         return <<<EOF
-// <html>
-// <head>
-// <title>Hello/Index</title>
-// <style>
-// body {font-size:16pt; color:999; }
-// h1 { font-size:100pt; text-aligin:right; color:#eee;
-//     margin:-40px 0px -50px 0px; }
-// </style>
-// </head>
-// <body>
-//    <h1>Index</h1>
-//    <p>これは、Helloコントローラーのindexアクションです。</p>
-// </body>
-// </html>
-// EOF;
+//         return view('hello.index');
 //     }
 // }
 
-
-//ルートパラメータの利用
-// class HelloController extends Controller
-// {
-//     public function index($id='noname', $pass='unknown') {
-//         return <<<EOF
-// <html>
-// <head>
-// <title>Hello/Index</title>
-// <style>
-// body {font-size:16pt; color:999; }
-// h1 { font-size:100pt; text-aligin:right; color:#eee;
-//     margin:-40px 0px -50px 0px; }
-// </style>
-// </head>
-// <body>
-//    <h1>Index</h1>
-//    <p>これは、Helloコントローラーのindexアクションです。</p>
-//    <ul>
-//         <li>ID: {$id}</li>
-//         <li>PASS: {$pass}</li>
-//    </ul>
-// </body>
-// </html>
-// EOF;
-//     }
-// }
-
-
-// //複数アクションの利用
-// global $head, $style, $body, $end;
-// $head = '<html><head>';
-// $style = <<<EOF
-// <style>
-// body {font-size:16pt; color:999; }
-// h1 { font-size:100pt; text-aligin:right; color:#eee;
-//     margin:-40px 0px -50px 0px; }
-// </style>
-// EOF;
-// $body = '</head><body>';
-// $end = '</body></html>';
-
-// function tag($tag, $txt){
-//     return "<{$tag}>" . $txt . "</{$tag}>";
-// }
-
+// //値をテンプレートに渡す
 // class HelloController extends Controller
 // {
 //     public function index() {
-//         global $head, $style, $body, $end;
-//         $html = $head . tag('title', 'Hello/Index') . $style .
-//              $body
-//              . tag('h1', 'Index') . tag('p', 'this is Index page')
-//              . '<a href="/hello/other">go to Other page</a>'
-//              . $end;
-//         return $html;
-//     }
-//     public function other() {
-//         global $head, $style, $body, $end;
-//         $html = $head . tag('title', 'Hello/Other') . $style .
-//              $body
-//              . tag('h1', 'Other') . tag('p', 'this is Other page')
-//              . $end;
-//         return $html;
+//         $data = ['msg'=>'これはコントローラーから渡されたメッセージです'];
+//         return view('hello.index',$data);
 //     }
 // }
 
-
-// //HelloControllerをシングルアクション化
+// //ルートパラメータをテンプレートに渡す アクションの修正
 // class HelloController extends Controller
 // {
-//     public function __invoke() {
-//         return <<<EOF
-//     <html>
-//     <head>
-//     <title>Hello</title>
-//     <style>
-//     body {font-size:16pt; color:999; }
-//     h1 { font-size:100pt; text-aligin:right; color:#eee;
-//         margin:-40px 0px -50px 0px; }
-//     </style>
-//     </head>
-//     <body>
-//        <h1>Single Action</h1>
-//        <p>これは、シングルアクションコントローラーのアクションです。</p>
-//     </body>
-//     </html>
-//     EOF;
+//     public function index($id='zero') 
+//     {
+//         $data = [
+//             'msg'=>'これはコントローラーから渡されたメッセージです',
+//             'id'=>$id
+//         ];
+//         return view('hello.index',$data);
 //     }
 // }
 
-
-//RequestおよびResponse
-use Illuminate\Http\Response;
-
+//クエリー文字列の利用
 class HelloController extends Controller
 {
-    public function index(Request $request, Response $response) {
-    $html = <<<EOF
-    <html>
-    <head>
-    <title>Hello/Index</title>
-    <style>
-    body {font-size:16pt; color:999; }
-    h1 { font-size:100pt; text-aligin:right; color:#eee;
-        margin:-40px 0px -50px 0px; }
-    </style>
-    </head>
-    <body>
-       <h1>Hello</h1>
-       <h3>Request</h3>
-       <pre>{$request}</pre>
-       <h3>Response</h3>
-       <pre>{$response}</pre>
-    </body>
-    </html>
-    EOF;
-        $response->setContent($html);
-        return $response;
+    public function index(Request $request) 
+    {
+        $data = [
+            'msg'=>'これはコントローラーから渡されたメッセージです',
+            'id'=>$request->id
+        ];
+        return view('hello.index',$data);
     }
 }
---
+++ src/routes/memoweb.php
@@ -0,0 +1,119 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// use Illuminate\Routing\Route;
+
+// Route::get(割り当てるアドレス,関数やコントローラーなど);
+// 下記はデフォルトで用意されているトップページのルート情報
+  Route::get('', function () {
+      return view('welcome'); 
+//         //return で返される値がそのアドレスにアクセスした際に表示される内容
+//           //今回の戻り値→ view(テンプレート名)  指定したテンプレートファイルをロードし、レンダリングして返す
+//           //view('welcome') でwelcome.blade.phpというテンプレートファイルをレンダリング表示
+//             //テンプレート ↑ は resourcesフォルダ内のviewsフォルダの中
+  });
+
+// Route::get('hello', function () {
+//     return '<html><body><h1>hello</h1><p>This is sample page.</p></body></html>'; 
+// });
+
+
+//ヒアドキュメントを使う  
+  //PHPで長文テキストを記述するのに使われるもの、<<<演算子を使い、リスト内に直接記述されたテキストをまとめて変数などに代入
+// $html = <<<EOF
+// <html>
+// <head>
+// <title>Hello</title>
+// <style>
+// body {font-size:16pt; color:999; }
+// h1 { font-size:100pt; text-aligin:right; color:#eee;
+//     margin:-40px 0px -50px 0px; }
+// </style>
+// </head>
+// <body>
+//    <h1>Hello</h1>
+//    <p>This is sample page.</p>
+//    <p>これは、サンプルで作ったページです。</p>
+// </body>
+// </html>
+// EOF;
+
+// Route::get('hello', function () use ($html) {
+//      return $html; 
+// });
+  //本格的なwebページを作る場合は別の方法がある
+
+
+//ルートパラメータの利用
+  //アクセスする際にパラメータを設置、
+// Route::get('hello/{msg}', function ($msg) {
+// $html = <<<EOF
+// <html>
+// <head>
+// <title>Hello</title>
+// <style>
+// body {font-size:16pt; color:999; }
+// h1 { font-size:100pt; text-aligin:right; color:#eee;
+//     margin:-40px 0px -50px 0px; }
+// </style>
+// </head>
+// <body>
+//    <h1>Hello</h1>
+//    <p>{$msg}</p>
+//    <p>これは、サンプルで作ったページです。</p>
+// </body>
+// </html>
+// EOF;
+//      return $html; 
+// });
+
+
+//今までは必須パラメータ
+//任意パラメータを使う⇒パラメータ名の末尾に？を付けて宣言、第二引数にデフォルト値を指定
+// Route::get('hello/{msg?}', function ($msg='no message.') {
+// $html = <<<EOF
+// <html>
+// <head>
+// <title>Hello</title>
+// <style>
+// body {font-size:16pt; color:999; }
+// h1 { font-size:100pt; text-aligin:right; color:#eee;
+//     margin:-40px 0px -50px 0px; }
+// </style>
+// </head>
+// <body>
+//    <h1>Hello</h1>
+//    <p>{$msg}</p>
+//    <p>これは、サンプルで作ったページです。</p>
+// </body>
+// </html>
+// EOF;
+//      return $html; 
+// });
+
+
+//HelloController.phpのためのルート情報の用意
+// Route::get('hello', 'HelloController@index');
+
+//ルートパラメータの利用による、ルート情報の修正
+// Route::get('hello/{id?}/{pass?}', 'HelloController@index');
+
+//複数アクションの利用による、ルート情報の修正
+// Route::get('hello', 'HelloController@index');
+// Route::get('hello/other', 'HelloController@other');
+
+//複数アクションの利用による、ルート情報の修正
+// Route::get('hello', 'HelloController');
+
+//RequestおよびResponseによる、ルート情報の修正
+// Route::get('hello', 'HelloController@index');
