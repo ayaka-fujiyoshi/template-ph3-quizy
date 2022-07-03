e71aa68545dc3f602ad26440815b597e23c8ac9e
{ayaka-fujiyoshi} <ayaka1712pome@gmail.com>
2022-07-04 01:36:01 +0900

    第四章完了

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -13,12 +13,27 @@ class HelloController extends Controller
 {
     public function index(Request $request) 
     {
-        return view('hello.index',['msg'=>'フォームを入力下さい。']);
+        //クッキーを読み書きする
+        if ($request->hasCookie('msg')) {
+            $msg = 'Cookkie: ' . $request->cookie('msg');
+        } else {
+            $msg = '※クッキーはありません。';
+        }
+        return view('hello.index',['msg'=>$msg]);
     }
 
     //use App\Http\Requests\HelloRequest;を追加
     public function post(HelloRequest $request)  
     {
-        return view('hello.index',['msg'=>'正しく入力されました！']);
+         //クッキーを読み書きする
+         $validate_rule = [
+            'msg'=>'required',
+         ];
+         $this->validate($request, $validate_rule);
+         $msg = $request->msg;
+         $response = response()->view('hello.index',
+         ['msg'=>'「' . $msg . '」をクッキーに保存しました。']);
+         $response->cookie('msg', $msg, 100);
+        return $response;
     }
 }
--- src/resources/views/hello/memo.blade.php
+++ src/resources/views/hello/memo.blade.php
@@ -234,3 +234,35 @@
       <tr><th></th><td><input type="submit" value="send"></td></tr>
   </table>
 </form>
+
+
+<p>{{$msg}}</p>
+  @if (count($errors) > 0)
+      <p>入力に問題があります。再入力してください。</p>
+  @endif
+<form method="POST" action="/hello">
+    <table>
+       @error ('name')
+           <tr>
+              <th>ERROR</th>
+              <td>{{$message}}</td>
+           </tr>
+       @enderror
+        <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+       @error ('mail')
+           <tr>
+              <th>ERROR</th>
+              <td>{{$message}}</td>
+           </tr>
+       @enderror
+        <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
+       @error ('age')
+           <tr>
+              <th>ERROR</th>
+              <td>{{$message}}</td>
+           </tr>
+       @enderror
+        <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
+        <tr><th></th><td><input type="submit" value="send"></td></tr>
+    </table>
+</form>
