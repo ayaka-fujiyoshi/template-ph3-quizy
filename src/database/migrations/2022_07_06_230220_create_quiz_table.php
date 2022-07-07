57e1d33db40c7449c46eef415eff892aa0918441
{ayaka-fujiyoshi} <ayaka1712pome@gmail.com>
2022-07-07 21:09:10 +0900

    migrateでテーブル作成

---
+++ src/database/migrations/2022_07_06_230220_create_quiz_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Schema::create('quiz', function (Blueprint $table) {
+        //     $table->bigIncrements('id');
+        //     $table->timestamps();
+        // });
+
+        // 大問テーブル
+        Schema::create('big_questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+        // 設問テーブル （写真）
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('big_question_id');
+            $table->string('image');
+            $table->timestamps();
+        });
+        // 選択肢テーブル
+        Schema::create('choices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('question_id');
+            $table->integer('selection_id');
+            $table->string('name');
+            $table->integer('valid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::dropIfExists('quiz');
+        Schema::dropIfExists('big_questions');
+        Schema::dropIfExists('questions');
+        Schema::dropIfExists('choices');
+    }
+}
