dfb8cbc023dfbff32f1646c8d770a774bcae7176
{ayaka-fujiyoshi} <ayaka1712pome@gmail.com>
2022-07-07 21:43:13 +0900

    seedingしてカラム挿入

--- src/database/seeds/QuizTableSeeder.php
+++ src/database/seeds/QuizTableSeeder.php
@@ -11,6 +11,91 @@ class QuizTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // 大問テーブル
+        $param_big = [
+            [
+                'name' => '東京の難読地名クイズ',
+            ],
+            [
+                'name' => '広島県の難読地名クイズ',
+            ],
+        ];
+        DB::table('big_questions')->insert($param_big);
+
+        // 設問テーブル （写真）
+        $param_questions = [
+            [
+                'big_question_id' => 1,
+                'image' => 'takanawa.png',
+            ],
+            [
+                'big_question_id' => 1,
+                'image' => 'kameido.png',
+            ],
+            [
+                'big_question_id' => 2,
+                'image' => 'mukainada.png',
+            ],
+        ];
+        DB::table('questions')->insert($param_questions);
+
+        // 選択肢テーブル
+        $param_choices = [
+            [
+                'question_id' => 1,
+                'selection_id' => 0,
+                'name' => 'たかなわ',
+                'valid' => 1,
+            ],
+            [
+                'question_id' => 1,
+                'selection_id' => 1,
+                'name' => 'たかわ',
+                'valid' => 0,
+            ],
+            [
+                'question_id' => 1,
+                'selection_id' => 2,
+                'name' => 'こうわ',
+                'valid' => 0,
+            ],
+            [
+                'question_id' => 2,
+                'selection_id' => 0,
+                'name' => 'かめと',
+                'valid' => 0,
+            ],
+            [
+                'question_id' => 2,
+                'selection_id' => 1,
+                'name' => 'かめど',
+                'valid' => 0,
+            ],
+            [
+                'question_id' => 2,
+                'selection_id' => 2,
+                'name' => 'かめいど',
+                'valid' => 1,
+            ],
+            [
+                'question_id' => 3,
+                'selection_id' => 0,
+                'name' => 'むこうひら',
+                'valid' => 0,
+            ],
+            [
+                'question_id' => 3,
+                'selection_id' => 1,
+                'name' => 'むきひら',
+                'valid' => 0,
+            ],
+            [
+                'question_id' => 3,
+                'selection_id' => 2,
+                'name' => 'むかいなだ',
+                'valid' => 1,
+            ],
+        ];
+        DB::table('choices')->insert($param_choices);
     }
 }
