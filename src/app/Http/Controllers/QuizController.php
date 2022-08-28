3b552683d1268036d0eb02b7ec63a00c9de65495
{ayaka-fujiyoshi} <ayaka1712pome@gmail.com>
2022-08-28 22:29:53 +0900

    モデルを使った表示できた

--- src/app/Http/Controllers/QuizController.php
+++ src/app/Http/Controllers/QuizController.php
@@ -5,9 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-use APP\BigQuestion;
-use APP\Question;
-use APP\Choice;
+use App\BigQuestion;
+use App\Question;
+use App\Choice;
 
 class QuizController extends Controller
 {
